026a2d2bc0bc3edda6aa0be6d6ca764b0723e1dd
Your Name Here <jennifernhf@gmail.com>
2020-07-12 11:17:18 -0700

    wip building out page structure

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,11 @@
 import React from "react";
+import Home from "../pages/Home";
 import "../../index.scss";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Header</h1>
-      </header>
+      <Home></Home>
     </div>
   );
 }
---
+++ src/components/pages/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Page from "./Page";
+import Header from "../container/Header";
+
+const Home = () => (
+  <Page>
+    <Header></Header>
+  </Page>
+);
+
+export default Home;
